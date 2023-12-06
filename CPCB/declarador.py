3ae5a176698c5e1d44cbe114863569233375ccea
Guilherme Akira Mori <akira.demenech@gmail.com>
2023-12-06 03:01:58 -0300

    Gerador de tex

---
+++ CPCB/declarador.py
@@ -0,0 +1,129 @@
+latex_declara = '''\\documentclass[12pt]{article}
+	\\usepackage[a4paper,landscape]{geometry}
+	\\usepackage[T1]{fontenc}
+	\\usepackage{ragged2e}'''
+
+latex_abre = '''
+\\begin{document}
+	\\thispagestyle{empty} %% sem número de página
+	\\sffamily %% sem serifa
+	\\justifying %% justificado
+	\\hyphenpenalty=0 %% evitar hifenização	
+	\\hfill \\vfill %% centralizar verticalmente
+	
+	
+	\\centerline{\\Huge Declaração}
+	\\large
+	
+	\\vfill
+	%\\bigskip
+''' 	
+
+latex_fecha = '''
+	\\vfill
+	%\\bigskip
+	
+	\\normalsize
+	Assinaturas
+	
+	\\vfill \\hfill %% centralizar verticalmente
+
+\\end{document}'''
+
+modelo_participante = '''
+Declaramos para os devidos fins que
+	%s
+	participou do 
+	\\textbf{12° Congresso Paranaense de Ciências Biomédicas},
+	%%promovido pelo curso de biomedicina
+	entre os dias 
+	13 e 15 de março de 2024,
+	como OUVINTE'''
+modelo_apresentador = '''
+	e APRESENTADOR(A) do trabalho intitulado
+	%s'''	
+modelo_horas = ''',
+	totalizando 
+	CARGA HORÁRIA 
+	horas de duração'''	
+modelo_minicurso = ''',
+incluindo o minicurso
+	%s
+'''
+modelo_minicursos = ''',
+incluindo os minicursos
+	%s
+	e 
+	%s
+'''	
+
+perfil_0 = 'Participante Simples'
+perfil_1 = 'Participante + 1 Minicurso'
+perfil_2 = 'Participante + 2 Minicursos'
+
+import csv
+
+arquivo = 'Inscritos CPCB 2024.xlsx - Geral.csv'
+
+planilha = [[col.strip() for col in ln] for ln in csv.reader(open(arquivo, 'r', encoding='utf-8'))]
+colunas = planilha.pop(0)    
+
+col_cidade = colunas.index('Cidade')
+col_cpf = colunas.index('CPF')
+col_rg = colunas.index('RG')
+col_telefone = colunas.index('Telefone')
+col_email = colunas.index('E-mail')
+col_nome = colunas.index('Nome')
+col_resumo = colunas.index('Resumo')
+col_perfil = colunas.index('Perfil')
+col_minicurso_1 = colunas.index('Minicurso 1')
+col_minicurso_2 = colunas.index('Minicurso 2')
+col_minicurso_conferido = colunas.index('Minicurso conferido')
+
+def algarismos (t):
+	a = ''
+	for d in t:
+		a += d * d.isdigit()
+	return a	
+
+for ln in planilha:
+	cidade = ln[col_cidade].upper()
+	nome = ln[col_nome].upper()
+	email = ln[col_email].lower()
+	
+	telefone = algarismos(ln[col_telefone])
+	
+	cpf = algarismos(ln[col_cpf])
+	rg = algarismos(ln[col_rg])
+
+	perfil = ln[col_perfil]
+	resumo = ln[col_resumo]
+	
+	print(cpf,rg,'\t',nome,email,'\t',perfil)
+
+	tipo = -1
+	if perfil_0 in perfil:
+		tipo = 0
+	elif perfil_1 in perfil:
+		tipo = 1
+	elif perfil_2 in perfil:
+		tipo = 2	
+	else:	
+		print('Perfil não identificado\n')
+
+	with open(f'{tipo} {nome} {cpf} {rg}.tex', 'w', encoding='utf-8') as tex:
+		print(latex_declara, file=tex)
+		print(latex_abre, file=tex)
+		print(end=(modelo_participante % nome), file=tex)
+		if len(resumo) > 1:
+			print(end=(modelo_apresentador % resumo), file=tex)
+		print(end=modelo_horas, file=tex)
+
+		if tipo == 1:
+			print(end=(modelo_minicurso % ln[col_minicurso_1]), file=tex)
+		elif tipo == 2:
+			print(end=(modelo_minicursos % (ln[col_minicurso_1], ln[col_minicurso_2])), file=tex)	
+
+		print('.\n\n', latex_fecha, '\n%%', cidade, '\n%%', email, '\n%%', telefone, file=tex)
+
+	
